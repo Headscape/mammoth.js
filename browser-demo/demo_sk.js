063aed21e7dc8e8d5da6a802cb54f4ae1de684ad
Sandra Koning <sandra.koning@headscape.co.uk>
2022-06-17 15:36:19 +0100

    add: test files for stylemap in browser demo

---
+++ browser-demo/demo_sk.js
@@ -0,0 +1,50 @@
+(function() {
+    document.getElementById("document")
+        .addEventListener("change", handleFileSelect, false);
+
+    function handleFileSelect(event) {
+        var options = {
+            styleMap: [
+                "p[style-name='Section Title'] => h1:fresh",
+                "p[style-name='Subsection Title'] => h2:fresh"
+            ]
+        };
+
+        readFileInputEventAsArrayBuffer(event, function(arrayBuffer) {
+            mammoth.convertToHtml({arrayBuffer: arrayBuffer})
+                .then(displayResult)
+                .done();
+        });
+    }
+
+    function displayResult(result) {
+        document.getElementById("output").innerHTML = result.value;
+
+        var messageHtml = result.messages.map(function(message) {
+            return '<li class="' + message.type + '">' + escapeHtml(message.message) + "</li>";
+        }).join("");
+
+        document.getElementById("messages").innerHTML = "<ul>" + messageHtml + "</ul>";
+    }
+
+    function readFileInputEventAsArrayBuffer(event, callback) {
+        var file = event.target.files[0];
+
+        var reader = new FileReader();
+
+        reader.onload = function(loadEvent) {
+            var arrayBuffer = loadEvent.target.result;
+            callback(arrayBuffer);
+        };
+
+        reader.readAsArrayBuffer(file);
+    }
+
+    function escapeHtml(value) {
+        return value
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+    }
+})();
---
+++ browser-demo/stylemap_sk.js
@@ -0,0 +1,6 @@
+p[style-name='Section Title'] => h1:fresh
+p[style-name='Subsection Title'] => h2:fresh
+p[style-name='__TEST_SMALLCAPS'] => h3:fresh
+p[style-name='toc 1'] => h4:fresh
+p[style-name='TOC_1'] => h5:fresh
+
