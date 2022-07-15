56605d30e1e80af592781e5250127635bd953c17
Sandra Koning <sandra.koning@headscape.co.uk>
2022-07-15 14:05:35 +0100

    add: mjlr header  & toc mapping

--- browser-demo/stylemap_sk.js
+++ browser-demo/stylemap_sk.js
@@ -5,6 +5,12 @@ var customStyleMap = [
     "p[style-name='Heading 3'] => h4:fresh",
     "p[style-name='Heading 4'] => h5:fresh",
     "p[style-name='Heading 5'] => h6:fresh",
+    "p[style-name='_SubHead1'] => h2:fresh",
+    "p[style-name='SubHead1'] => h2:fresh",
+    "p[style-name='_SubHead2'] => h3:fresh",
+    "p[style-name='_SubHead3'] => h4:fresh",
+    "p[style-name='_SubHead4'] => h5:fresh",
+    "p[style-name='_TableHeading'] => h3.table-heading:fresh",
     "p[style-name='Head1'] => h2:fresh",
     "p[style-name='Head2'] => h3:fresh",
     "p[style-name='Section Title'] => h2:fresh",
@@ -12,12 +18,13 @@ var customStyleMap = [
     "p[style-name='_Head2-Notes'] => h3:fresh",
     "p[style-name^='toc'] => !",
     "p[style-name^='TOC'] => !",
+    "p[style-name^='_Toc'] => !",
     "small-caps => span.small-caps"
 ];
 
 function transformAllZWAdobeF(run) {
     if (run.font && run.font.toLowerCase() == 'zwadobef') {
-        return {}
+        return {};
     } else {
         return run;
     }
